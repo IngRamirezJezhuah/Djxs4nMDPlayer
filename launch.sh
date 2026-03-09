f8393c37fa91024710c958c75cf0b079aa82791c
IngRamirezJezhuah <ramierzalvaradodavidjezhuah@gmail.com>
2026-03-09 12:57:32 -0600

    Version 1.2 + fixes

--- launch.sh
+++ launch.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
-#traductor de los colores de wallus , es que uso hyprland
-sed 's/ = rgb(\(.*\))/: #\1;/g' ~/.config/hypr/wallust/wallust-hyprland.conf > "$(dirname "$0")/music-dot/eww/colors.scss"
+
+#Nota esta madre la hice basandome en el uso de un entorno en hyprland, como tengo chingaderas que son, hyprp el rofi, y otras weas asi use los colores y esas mamadas para que se viera aca que chulo que precioso, luego trabajare en una que este en base x11 para quienes no usen un gestor de ventana, pero ahi va
+
+#El traductor de los colores de wallus , es que uso hyprland y asi usamos los colores del wallpaper, ojo si tu tienes las configuraciones de esa wea en otra carpeta porfa bro modificala a la que tu tienes
+grep "=" ~/.config/hypr/wallust/wallust-hyprland.conf | sed 's/ = rgb(\(.*\))/: #\1;/g' | tr -d '\r' > "$(dirname "$0")/music-dot/eww/colors.scss"
+#↳ en español lo que hace es que busca el archivo en tus config de color y una vez lo encuentra a partir de eso va a crear un archico copia de ese en la carpeta de este proyecto y le da el nombre de colors.scss por si le quieres meter mano ahi puedes buscarlo
 
 #para que no se pierda playerctl y cambie entre web y nativo uso esto
-playerctld daemon &
+#playerctld daemon &
+ if ! pidof eww > /dev/null; then
+   # ↳ esta madre lo que hace es que revisa si el eww esta chambeando
+   playerctld daemon & 
+   # ↳ entonces si esta chambeando pues pide al reproductor que busque el anterior que ya estaba haciendo chamba de musica y lo pone para reproducir
+   eww daemon &
+   #↳ ahora si ya prende el cacharro a chambear
+   sleep 0.5
+   #↳hay que darle tiempo a que se muestre segun un profe eso es bueno como experiencia de user algo asi decia
+ fi
 
-#Esta chingadera le da muerte a todos los procesos viejos de eww
-killall eww 2>/dev/null
-
-#la madre que levanta el daemon
-eww daemon &
-
-#no se como sean las compus asi que le dare un refresco para que abra la ventana
-
-sleep 0.5
-eww open music_win
+#Esta chingadera le da muerte a todos los procesos viejos de eww : killall eww 2>/dev/null <-- la guardo por si acaso para futuros casos
+#no se como sean las compus de otros asi que le dare un refresco para que abra la ventana y la recargue con el cambio de pantalla
+eww reload
+eww open --toggle music_win

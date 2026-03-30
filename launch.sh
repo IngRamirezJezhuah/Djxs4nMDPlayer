de85b69d4994147f566612169c28d00820fbe802
IngRamirezJezhuah <ramierzalvaradodavidjezhuah@gmail.com>
2026-03-30 17:19:39 -0600

    implementacion de dots 1.0

--- launch.sh
+++ launch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-
-#Nota esta madre la hice basandome en el uso de un entorno en hyprland, como tengo chingaderas que son, hyprp el rofi, y otras weas asi use los colores y esas mamadas para que se viera aca que chulo que precioso, luego trabajare en una que este en base x11 para quienes no usen un gestor de ventana, pero ahi va
+#======================================================================
+#Nota esta madre la hice basandome en el uso de un entorno en hyprland,como tengo chingaderas que son, hyprp el rofi, y otras weas asi use los colores y esas mamadas para que se viera aca que chulo que precioso, luego trabajare en una que este en base x11 para quienes no usen un gestor de ventana, pero ahi va
+#======================================================================
+#Aqui defino la ruta del proyecto de manera dinamica por que pues uso un chingo de archivos que usan eww y para no batallar al ejecutar dirnamw "0" apunta a la carpeta donde esta el launch.sh
+#=====================================================================
+PROJECT_DIR="$(dirname "$0")"
+CFG_DIR="$PROJECT_DIR/music-dot/eww"
 
 #El traductor de los colores de wallus , es que uso hyprland y asi usamos los colores del wallpaper, ojo si tu tienes las configuraciones de esa wea en otra carpeta porfa bro modificala a la que tu tienes
 grep "=" ~/.config/hypr/wallust/wallust-hyprland.conf | sed 's/ = rgb(\(.*\))/: #\1;/g' | tr -d '\r' > "$(dirname "$0")/music-dot/eww/colors.scss"
-#↳ en español lo que hace es que busca el archivo en tus config de color y una vez lo encuentra a partir de eso va a crear un archico copia de ese en la carpeta de este proyecto y le da el nombre de colors.scss por si le quieres meter mano ahi puedes buscarlo
+#↳ en español lo que hace es que busca el archivo en tus config de color 
+#y una vez lo encuentra a partir de eso va a crear un archico copia de 
+#ese en la carpeta de este proyecto y le da el nombre de colors.scss 
+#por si le quieres meter mano ahi puedes buscarlo 
 
 #para que no se pierda playerctl y cambie entre web y nativo uso esto
 #killall eww 2>/dev/null
@@ -13,7 +21,7 @@ grep "=" ~/.config/hypr/wallust/wallust-hyprland.conf | sed 's/ = rgb(\(.*\))/: 
    # ↳ esta madre lo que hace es que revisa si el eww esta chambeando
    playerctld daemon & 
    # ↳ entonces si esta chambeando pues pide al reproductor que busque el anterior que ya estaba haciendo chamba de musica y lo pone para reproducir
-   eww daemon &
+   eww --config "$CFG_DIR" daemon &
    #↳ ahora si ya prende el cacharro a chambear
    sleep 0.5
    #↳hay que darle tiempo a que se muestre segun un profe eso es bueno como experiencia de user algo asi decia
@@ -21,5 +29,6 @@ grep "=" ~/.config/hypr/wallust/wallust-hyprland.conf | sed 's/ = rgb(\(.*\))/: 
 
 #Esta chingadera le da muerte a todos los procesos viejos de eww : killall eww 2>/dev/null <-- la guardo por si acaso para futuros casos
 #no se como sean las compus de otros asi que le dare un refresco para que abra la ventana y la recargue con el cambio de pantalla
-eww reload
-eww open --toggle music_win
+eww --config "$CFG_DIR" reload
+eww --config "$CFG_DIR" open --toggle music_win
+
--- music-dot/scripts/get_metadata.sh
+++ music-dot/scripts/get_metadata.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #esto administra y realiza los cambios en el reproductor y regresa en un json, es que me da hueva hacer otra wea para eso hehe
 
-
 #imagen por defecto si no hay caratula
 DEFAULT_COVER="$(pwd)/../eww/default.png"
 COVER="/tmp/music-dot-cover.png"
@@ -14,14 +13,13 @@ get_data() {
   art_url=$(playerctl metadata mpris:artUrl 2>/dev/null)
 
   #Con la nueva mare del cava ahora extraigo los tiempos brutos
-  pos=$(playerctl position 2>/dev/null || cut -d'.' -f1)
+  pos=$(playerctl position 2>/dev/null | cut -d'.' -f1)
   len_us=$(playerctl metadata mpris:length 2>/dev/null)
   #segun lei puedo usar awk para convertirlo a segundo entero por que len_raw viene en microsegundos
   #position=$(echo "$post_raw" | awk '{print int($1)}')
   #length=$(echo "$len_us" | awk '{print int($1/1000000)}')
   [[ -z "$pos" ]] && pos=0
   if [[ -z "$len_us" ]]; then len=100; else len=$((len_us / 1000000)); fi
-
   #logica de la caratula pa que se muestre pues
   if [[ -z  "$art_url" ]]; then
       cp "$DEFAULT_COVER" "$COVER" 2>/dev/null || touch "$COVER"
@@ -32,21 +30,11 @@ get_data() {
       cp "${art_url#file://}" "$COVER"
   fi
   #Esta chingadera genera un json limpio con un jq a prueba de errores por que estoy hasta la chingada de depurar typos a la verga
-  jq --unbuffered -c -n --arg title "$title" --arg artist "$artist" --arg status "$status" --arg cover "$COVER" --arg pos "$position" --arg len "$length" '{title: $title, artist: $artist, status: $status, cover: $cover, position: $pos, length: $len}'
+  jq --unbuffered -c -n --arg title "$title" --arg artist "$artist" --arg status "$status" --arg cover "$COVER" --arg pos "$pos" --arg len "$len" '{title: $title, artist: $artist, status: $status, cover: $cover, position: $pos, length: $len}'
 }
 
 while true; do
   get_data
   sleep 0.5
 done
-
-#forma de lectura de jq
-#playerctl metadata --format '{
-#  "title": "{{tittle}}", 
-#  "artist": "{{artist}}",
-#  "status": "{{status}}",
-#  "art": "{{mpris:artUrl}}"
-#}' --follow | while read -r line: do
-#  echo "$line"
-#done
 

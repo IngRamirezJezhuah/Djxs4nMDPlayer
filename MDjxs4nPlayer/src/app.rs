656c6e0e28e794f9790a06c3fab950b3ff2fb90b
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2026-09-07 18:50:37 -0600

    implementacion de audio.rs a app.rs

--- MDjxs4nPlayer/src/app.rs
+++ MDjxs4nPlayer/src/app.rs
@@ -23,6 +23,14 @@ struct LoadArgs {
     path: String,
 }
 
+#[derive(Serialize, Deserialize, Clone, Debug)]
+struct MusicaData {
+    pub title: String,
+    pub artist: String,
+    pub cover_ui : String,
+    pub status: String,
+}
+
 #[derive(Serialize, Deserialize, Clone)]
 struct TrackMetadata {
     title: String,
@@ -35,14 +43,41 @@ struct TrackMetadata {
 pub fn Mdplayer() -> impl IntoView {
 
     let (is_playing, set_is_playing) = signal(false);
-    let (title, set_title) = signal("Titulo".to_string());
+    let (title, set_title) = signal("Cargandp...".to_string());
     let (artist, set_artist) = signal("Nom Artista".to_string());
     let (cover_url, set_cover_url) = signal("/public/cover.png".to_string());
 
-    // Acción para alternar Play / Pausa
+
+    let update_metadata = move || {
+        spawn_local(async move {
+            let res = invoke("get_metada", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            if let Ok(meta) = serde_wasm_bindgen::from_value::<MusicaData>(res) {
+                set_title.set(meta.title);
+                set_artist.set(meta.artist);
+                if !meta.cover_ui.is_empty() {
+                    set_cover_url.set(meta.cover_ui);
+                }
+                set_is_playing.set(meta.status == "Playing");
+            }
+        });
+    };
+
+    update_metadata();
+
+    /* Acción para alternar Play / Pausa
     let toggle_play = move |_| {
         leptos::task::spawn_local(async move {
             let res = invoke("play_pause", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            if let Ok(state) = serde_wasm_bindgen::from_value::<bool>(res) {
+                set_is_playing.set(state);
+            }
+        });
+    };
+    */
+
+    let toggle_play = move |_| {
+        spawn_local(async move {
+            let res = invoke("play_plause", serde_wasm_bindgen::to_value(&()).unwrap()).await;
             if let Ok(state) = serde_wasm_bindgen::from_value::<bool>(res) {
                 set_is_playing.set(state);
             }
@@ -52,11 +87,43 @@ pub fn Mdplayer() -> impl IntoView {
     // Acción para adelantar (+10s) o retrasar (-10s)
     let seek = move |secs: f64| {
         move |_| {
-            leptos::task::spawn_local(async move {
+            spawn_local(async move {
                 let args = serde_wasm_bindgen::to_value(&SeekArgs { seconds: secs }).unwrap();
                 let _ = invoke("seek_audio", args).await;
             });
         }
+    };
+
+    let next_track = move |_| {
+        spawn_local(async move{
+            let _ = invoke("next_track", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            // Actualizamos los metadatos inmediatamente despueus de cambiar
+            let res = invoke("get_metadata", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            if let Ok(meta) = serde_wasm_bindgen::from_value::<MusicaData>(res) {
+                set_title.set(meta.title);
+                set_artist.set(meta.artist);
+                if !meta.cover_ui.is_empty() {
+                    set_cover_url.set(meta.cover_ui);
+                }
+                set_is_playing.set(meta.status == "Playing");
+            }
+        });
+    };
+
+    let previous_track = move |_| {
+        spawn_local(async move {
+            let _ = invoke("previous_track", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            // Actualizamos los metadatos inmediatamente después de cambiar
+            let res = invoke("get_metadata", serde_wasm_bindgen::to_value(&()).unwrap()).await;
+            if let Ok(meta) = serde_wasm_bindgen::from_value::<MusicaData>(res) {
+                set_title.set(meta.title);
+                set_artist.set(meta.artist);
+                if !meta.cover_ui.is_empty() {
+                    set_cover_url.set(meta.cover_ui);
+                }
+                set_is_playing.set(meta.status == "Playing");
+            }
+        });
     };
 
     view! {
@@ -85,14 +152,14 @@ pub fn Mdplayer() -> impl IntoView {
 
                     {/* Botones de control */}
                     <button class="btn-trsp" on:click=seek(-10.0)>"↺ 10s"</button>
-                    <button class="btn-trsp">"◀"</button>
+                    <button class="btn-trsp" on:click=previous_track>"◀"</button>
                     <button class="btn" on:click=toggle_play>
                         {move || if is_playing.get() { "▐▐" } else { "▷" }}
                     </button>
-                    <button class="btn-trsp">"▶"</button>
+                    <button class="btn-trsp" on:click=next_track>"▶"</button>
                     <button class="btn-trsp" on:click=seek(10.0)>"10s ↻"</button>
-                    <div>----- barra tiempo ---</div>
-                    <p>...</p>
+                    <div>"----- barra tiempo ---"</div>
+                    <p>"..."</p>
                 </div>
             </div>
                 

5f10a33dac43a2684789f4f0fb49a06b7a8d3239
IngRamirezJezhuah <ramirezalvaradodavidjezhuah@gmail.com>
2026-09-11 14:54:05 -0600

    fix error de audio y music_player

--- MDjxs4nPlayer/src-tauri/src/music_player.rs
+++ MDjxs4nPlayer/src-tauri/src/music_player.rs
@@ -1,5 +1,5 @@
-// archivo de configuracion donde se alberrga el pryecto de usica
-use tauri::{AppHandle, Emitter};
+/* archivo de configuracion donde se alberrga el pryecto de usica
+use tauri::{AppHandle, Emitter, async_runtime::TokioHandle};
 use serde::Serialize;
 use std::time::Duration;
 
@@ -17,12 +17,40 @@ async fn play_Audio(app: AppHandle) {
     
     while current <= total_duracion {
         //total_duracion::time::sleep(std::time::Duration::from_millis(500)).await;
-        tokio::time::sleep(Duration::from_millis(500)).await;
+        TokioHandle::time::sleep(Duration::from_millis(500)).await;
         current += 0.5;
 
         let _ = app.emit("audio-progress", AudioProgress {
             current,
             total : total_duracion,
         });
+    }
+}
+*/
+/* version sin romper */
+// archivo de configuracion donde se alberrga el pryecto de usica
+use tauri::{AppHandle, Emitter};
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+struct AudioProgress {
+    current : f64,
+    total: f64,
+
+}
+
+#[tauri::command]
+async fn play_Audio(app: AppHandle) {
+    let total_duracion = 180.0;
+    let mut current = 0.0;
+    
+    while current <= total_duracion {
+        //total_duracion::time::sleep(std::time::Duration::from_millis(500)).await;
+        current += 0.5;
+
+        app.emit("audio-progress", AudioProgress {
+            current,
+            total : total_duracion,
+        }).unwrap();
     }
 }
